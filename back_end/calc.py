a156cf431d0f6b8b4c88348ac569f9913142b9e3
santiago-cujaban <79294248+santiago-cujaban@users.noreply.github.com>
2023-09-29 04:52:51 -0500

    001 - Improved names and new functions
    TODO:
    - Create tables based on data
    - Add front-end
    - Add comments on code

--- back_end/calc.py
+++ back_end/calc.py
@@ -1,122 +1,127 @@
 import copy
 
-"""
-Invierte el orden de los nodes dentro de los grafos
-"""
-
-def reverse_order(graph: dict):
-    return dict(reversed(list(graph.items())))    
-
-
-"""
-Imprime un diccionario con mejor orden
-"""
-def print_graph(graph: dict):
-    for i, j in graph.items():
-        print(i,":",j)
-
-"""
-Agrega las etapas indicadas despues de encontrar las soluciones
-"""
-def add_phases(graph: dict, phases: dict):
-    for key, phase in phases.items():
-        graph.get(key)['etapa'] = phase
+def solver_walker(graph: dict, phases: dict):
+    validate_graph(graph, phases)
+    reversed_graph = dict(reversed(list(graph.items()))) # Invierte el orden A -> Z por Z -> A
+    solved_graph = copy.deepcopy(walk_graph(reversed_graph, graph, phases))  # Solucion del problema
+    solution_best_path = find_best_paths_limited(solved_graph)  # Posibles Rutas Optimas
     
+    print(solution_best_path)   # Imprime las posibles rutas optimas
+    for i, j in solved_graph.items(): print(f'{i} : {j}') # Imprime Grafo Solucionado
 
-"""
-Separa el diccionario por etapa en una lista con diccionarios
-"""
-def separate_to_tables():
-    ...
+def validate_graph(graph: dict, phases:dict):
+    graph_c = copy.deepcopy(graph)
 
-"""
-Recorre el node invertido y realiza las operaciones para encontrar
-las sumas correspondientes al mejor camino.
-"""
+    if len(list(graph_c.keys())) == 0 or len(list(phases.keys())) == 0:
+        raise Exception('DICCIONARIO Vacio')
 
-def walk_graph(graph: dict, og: dict):
-    og_copy = copy.deepcopy(og)
-    for node, links in graph.items():
+    last_phase = phases[list(graph_c.keys())[-1]]
+    for key, value in graph_c.items():
+        f_phase = phases[key]
+        for k, v in value.items():
+            k_phase = phases.get(k, last_phase + 1)
+            if f_phase + 1 != k_phase:
+                raise Exception('Mala Conexión')
 
-        if len(og_copy.get(node).keys()) == 1: 
-            key_node = list(graph.get(node).keys())[0]
-            key_graph = graph.get(key_node,0)
+
+def walk_graph(reversed_graph: dict, original_graph: dict, phases: dict):
+
+    graph_copy = copy.deepcopy(original_graph) # Copia del grafo original
+    for node, _ in reversed_graph.items(): # Recorre el grafo inverso
+
+        if len(graph_copy.get(node).keys()) == 1:  # Si el nodo solo tiene una conexión
             sum_data = {}
+            key_node = list(reversed_graph.get(node).keys())[0]
+            key_graph = reversed_graph.get(key_node, 0)
 
-            if key_graph == 0 :
-                graph[node][f'Sum{node}'] = graph.get(node).get(key_node) + key_graph
-                sum_data[key_node] = graph.get(node).get(key_node) + key_graph
+            if key_graph == 0:
+                reversed_graph[node][f'f*(s){node}'] = reversed_graph.get(node).get(key_node) + key_graph
+                sum_data[key_node] = reversed_graph.get(node).get(key_node) + key_graph
             else:
-                graph[node][f'Sum{node}'] = graph.get(node).get(key_node) + key_graph.get(f'Sum{key_node}')
-                sum_data[key_node] = graph.get(node).get(key_node) + key_graph.get(f'Sum{key_node}')
+                reversed_graph[node][f'f*(s){node}'] = reversed_graph.get(node).get(key_node) + key_graph.get(f'f*(s){key_node}')
+                sum_data[key_node] = reversed_graph.get(node).get(key_node) + key_graph.get(f'f*(s){key_node}')
 
-            graph[node]['nodes'] = sum_data
+            reversed_graph[node]['f(s,x)'] = sum_data
 
-        if len(og_copy.get(node).keys()) > 1:
-            keys = list(graph.get(node).keys())
+        if len(graph_copy.get(node).keys()) > 1:   # Si el nodo tiene más de una conexión
             sum_data = {}
+            keys = list(reversed_graph.get(node).keys())
 
             for key in keys:
-                in_key = graph.get(node).get(key)
-                graph_key = graph.get(key).get(f'Sum{key}')
+                in_key = reversed_graph.get(node).get(key)
+                graph_key = reversed_graph.get(key).get(f'f*(s){key}')
                 sum_data[key] = in_key+graph_key
 
-            graph[node][f'Sum{node}'] = sum_data.get(min(sum_data, key=lambda k: sum_data[k]))
-            graph[node]['nodes'] = sum_data
+            reversed_graph[node][f'f*(s){node}'] = sum_data.get(min(sum_data, key=lambda k: sum_data[k]))
+            reversed_graph[node]['f(s,x)'] = sum_data
 
-    return graph
-
-if __name__=="__main__":
-
-    """ NODOS 4 ETAPAS
-    '1':{'2':2,'3':4,'4':3},
-        '2':{'5':7,'6':4,'7':6},
-        '3':{'5':3,'6':2,'7':4},
-        '4':{'5':4,'6':1,'7':5},
-        '5':{'8':1,'9':4},
-        '6':{'8':6,'9':3},
-        '7':{'8':3,'9':3},
-        '8':{'10':3},
-        '9':{'10':4}
-    ...
-    fases
-    '1':1,
-        '2':2,
-        '3':2,
-        '4':2,
-        '5':3,
-        '6':3,
-        '7':3,
-        '8':4,
-        '9':4
-    """
-    graph_og= {
-        '1':{'2':2,'3':4,'4':3},
-        '2':{'5':7,'6':4,'7':6},
-        '3':{'5':3,'6':2,'7':4},
-        '4':{'5':4,'6':1,'7':5},
-        '5':{'8':1,'9':4},
-        '6':{'8':6,'9':3},
-        '7':{'8':3,'9':3},
-        '8':{'10':3},
-        '9':{'10':4}   
-    }
-
-    phases = {
-        '1':1,
-        '2':2,
-        '3':2,
-        '4':2,
-        '5':3,
-        '6':3,
-        '7':3,
-        '8':4,
-        '9':4
-    }
-    
-    graph_inv = reverse_order(graph_og) # Devuelve el grafo invertido
-    solved_graph = copy.deepcopy(walk_graph(graph_inv, graph_og)) # Resuelve las sumas de caminos
-    add_phases(solved_graph, phases) # Agrega las fases indicadas por el usuario
-    print_graph(solved_graph)
+    # Elimina los números de la llave f*(s)
+    new_dictionary = {}
+    for key, value in reversed_graph.items():
+        new_value = {}
+        for subkey, subvalue in value.items():
+            new_subkey = subkey.replace(f'f*(s){key}', 'f*(s)')
+            new_value[new_subkey] = subvalue
+        new_dictionary[key] = new_value
+    reversed_graph = new_dictionary
     
 
+    find_path(reversed_graph)   # Agrega X* de cada S (nodo)
+    for key, phase in phases.items(): reversed_graph.get(key)['etapa'] = phase   #Agrega etapa correspondiente
+
+    return reversed_graph
+
+
+def find_path(graph: dict):
+
+    for node, _ in graph.items():
+        nodes_found = []
+        best_weight = graph.get(node).get('f*(s)')
+        nodes_dict = graph.get(node).get('f(s,x)')
+        
+        for key, value in nodes_dict.items():
+            if value == best_weight:
+                nodes_found.append(key)
+        graph[node]['x*'] = nodes_found
+
+
+def find_best_paths_limited(graph: dict):
+    graph_c = dict(reversed(list(copy.deepcopy(graph).items())))
+    first_node = list(graph_c.keys())[0] # Nodo Inicial
+    start = graph_c[first_node]['x*'] # Caminos posibles inicio
+    last_node = graph_c[list(graph_c.keys())[-1]]['x*'][0] # Nodo Final
+    
+    best_paths = []
+    
+    for step in start:
+        path = []
+        path.append(first_node)
+        walk = step
+        while walk != last_node:
+            path.append(walk)
+            walk = graph_c[walk]['x*'][0]
+        path.append(last_node)
+        best_paths.append(path)
+    
+    return best_paths
+
+
+if __name__ == "__main__":
+
+    #5 ETAPAS
+    #graph = 'A':{'B':2,'C':3, 'D':4}, 'B':{'E':3}, 'C':{'E':4, 'F':7}, 'D':{'F':5}, 'E':{'G':2,'H':5}, 'F':{'G':3}, 'G':{'I':1,'K':6}, 'H':{'I':4,'J':3}, 'I':{'L':1}, 'J':{'L':2}, 'K':{'L':3},
+    #phases = 'A':1, 'B':2, 'C':2, 'D':2, 'E':3, 'F':3, 'G':4, 'H':4, 'I':5, 'J':5, 'K':5
+
+    #4 ETAPAS
+    #graph = '1':{'2':2,'3':4,'4':3}, '2':{'5':7,'6':4,'7':6}, '3':{'5':3,'6':2,'7':4}, '4':{'5':4,'6':1,'7':5}, '5':{'8':1,'9':4}, '6':{'8':6,'9':3}, '7':{'8':3,'9':3}, '8':{'10':3}, '9':{'10':4}
+    #phases = '1':1, '2':2, '3':2, '4':2, '5':3, '6':3, '7':3, '8':4, '9':4
+    
+    #3 ETAPAS
+    #graph = '1':{'2':7,'3':8,'4':5}, '2':{'5':12}, '3':{'5':8,'6':9}, '4':{'5':7,'6':13}, '5':{'7':9}, '6':{'7':6}
+    #phases = '1':1, '2':2, '3':2, '4':2, '5':3, '6':3
+    
+
+    graph = {}
+    phases = {}
+
+    solver_walker(graph, phases)
--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,24 +1,6 @@
-import { useState } from "react";
 import style from "./styles/App.module.css";
 
 function App() {
-  const [etapas, setEtapas] = useState("");
-  const [conexiones, setConexiones] = useState("");
-
-  function sendInfo() {
-    const conexionesObj = JSON.parse(conexiones);
-    const etapasObj = JSON.parse(etapas);
-
-    fetch("http://localhost:8000/data", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ conexiones: conexionesObj, etapas: etapasObj }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log("Respuesta del servidor:", data))
-      .catch((error) => console.error("Error al enviar la solicitud:", error));
-  }
-
   return (
     <>
       <h1 style={{ textAlign: "center" }}>
@@ -26,25 +8,6 @@ function App() {
       </h1>
       <hr />
 
-      <div className={style.container}>
-        <div className={style.mini}>
-          <label>Ingrese nodos y sus conexiones</label>
-          <input
-            type="text"
-            onChange={(e) => setConexiones(e.target.value)}
-            placeholder='{"1":{"2":2, "3":4, "4":3}, ...}'
-          />
-        </div>
-        <div className={style.mini}>
-          <label>Ingrese las etapas de los nodos</label>
-          <input
-            type="text"
-            onChange={(e) => setEtapas(e.target.value)}
-            placeholder='{"1":1, "2":2, ...}'
-          />
-        </div>
-        <button onClick={sendInfo}>Solucionar!</button>
-      </div>
       <hr />
     </>
   );
