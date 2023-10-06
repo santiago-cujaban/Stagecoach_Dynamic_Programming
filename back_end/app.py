52bc53f8cba047e6d34cc72fe8032d00889aa952
santiago-cujaban <79294248+santiago-cujaban@users.noreply.github.com>
2023-10-06 02:04:14 -0500

    004 - Better CSS and Documentation
    Added Comments and "better" styles

--- back_end/app.py
+++ back_end/app.py
@@ -1,10 +1,14 @@
 # Iniciar con uvicorn app:app --reload
 import json
-from calc import solver_walker
+from calc import solver
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+"""
+CONFIGURACION PARA ENVIO DE RESPUESTAS Y SOLICITUDES AL FRONTEND.
+"""
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,14 +17,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+"""
+METODO POST
+    Recibe:
+        - JSON con conexiones de los nodos.
+        - JSON con etapas de los nodo.
+    Responde:
+        - JSON con la solución del problema y los caminos optimos
+        - Error si
+            - No se puede enviar o recibir los datos.
+"""
+
+
 @app.post('/data')
 async def send(request: Request):
     try:
         data = await request.json()
         conexiones = json.loads(data.get("conexiones"))
         etapas = json.loads(data.get("etapas"))
-        solucion, camino = solver_walker(conexiones, etapas)        
-        response_data = {"solucion": solucion, "camino":camino}
+        # Llama a la funcion que soluciona el problema en calc.py
+        solucion, camino = solver(conexiones, etapas)
+        print(solucion, camino)
+        response_data = {"solucion": solucion, "camino": camino}
         return response_data
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error al procesar los datos: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Error al procesar los datos: {str(e)}")
--- front_end/main.js
+++ front_end/main.js
@@ -1,26 +1,38 @@
 var maxNodes;
 var nodes = {};
 var phases = {};
+var errorWasAdded = false;
 
 // Crea la lista adjacente visualmente
 function createAdjacentList() {
+  nodes = {};
+  phases = {};
   maxNodes = parseInt(document.getElementById("numNodes").value);
   var spaceAL = document.getElementById("AdjacentList");
   spaceAL.innerHTML = "";
+  document.getElementById("cy").style.width = "50%";
+  spaceAL.style.display = "flex";
 
   for (var i = 0; i < maxNodes; i++) {
     var label = document.createElement("p");
     var input_phase = document.createElement("input");
-    label.textContent = i + "->";
-    input_phase.placeholder = "Num Etapa nodo: " + i;
+    label.textContent = "Conexiones del Nodo " + i;
+    input_phase.placeholder = "Etapa del nodo: " + i;
     input_phase.id = i;
+    input_phase.style.backgroundColor = "#e3b23c";
+    input_phase.autocomplete = "off";
     spaceAL.appendChild(label);
     spaceAL.appendChild(input_phase);
 
     for (var j = i + 1; j <= maxNodes; j++) {
       var input = document.createElement("input");
-      input.placeholder = i + " -> " + j;
-      input.id = i + "" + j;
+      input.placeholder = "Nodo " + i + " -> Nodo " + j;
+      input.id = i + " " + j;
+      input.autocomplete = "off";
+      input.addEventListener("focus", function () {
+        // Cambia los estilos cuando el input recibe el foco
+        input.style.backgroundColor = "lightblue";
+      });
       spaceAL.appendChild(input);
     }
   }
@@ -34,6 +46,37 @@ function createAdjacentList() {
   spaceAL.appendChild(solve_button);
 }
 
+// Soluciona el problema obtenido en forma de texto
+function createByText() {
+  adjList = document.getElementById("AdjacentList");
+  adjList.style.display = "none";
+  cyDiv = document.getElementById("cy");
+  cyDiv.style.width = "100%";
+  cyDiv.style.height = "40vh";
+  nodes_text = document.getElementById("Nodesbytext").value;
+  phases_text = document.getElementById("Phasesbytext").value;
+  nodes = JSON.parse(nodes_text);
+  phases = JSON.parse(phases_text);
+  infoSpace = document.getElementById("Info");
+  infoSpace.innerHTML = "";
+  p = document.createElement("p");
+
+  text_info =
+    "************************************************************<br>" +
+    "<b>SE ENVIO LA SIGUIENTE INFORMACION, COPIA ANTES DE REFRESCAR</b><br>" +
+    "************************************************************<br>" +
+    "<br><b>!! NODOS !!</b><br>" +
+    JSON.stringify(nodes) +
+    "<br><b>!! ETAPAS !!</b><br>" +
+    JSON.stringify(phases) +
+    "<br>";
+
+  p.innerHTML = text_info;
+  infoSpace.appendChild(p);
+  sendSolver();
+  createGraphView();
+}
+
 // Toma el valor de cada elemento y crea un objeto
 function createObject() {
   maxNodes = parseInt(document.getElementById("numNodes").value);
@@ -44,30 +87,52 @@ function createObject() {
 
     phases[i] = parseInt(phaseValue);
     for (var j = i + 1; j <= maxNodes; j++) {
-      var inputValue = document.getElementById(i + "" + j).value;
-      if (!isNaN(inputValue)) {
+      var inputValue = document.getElementById(i + " " + j).value;
+      if (!isNaN(inputValue) && inputValue !== "") {
         nodes[i][j] = parseInt(inputValue);
+        document.getElementById(i + " " + j).style.backgroundColor = "white";
       } else {
-        element = document.getElementById(i + "" + j);
-        element.style.display = "none";
+        document.getElementById(i + " " + j).style.backgroundColor = "gray";
+        document.getElementById(i + " " + j).style.display = "none";
       }
     }
   }
+  infoSpace = document.getElementById("Info");
+  infoSpace.innerHTML = "";
+  p = document.createElement("p");
+
+  text_info =
+    "************************************************************<br>" +
+    "<b>SE ENVIO LA SIGUIENTE INFORMACION, COPIA ANTES DE REFRESCAR</b><br>" +
+    "************************************************************<br>" +
+    "<br><b>!! NODOS !!</b><br>" +
+    JSON.stringify(nodes) +
+    "<br><b>!! ETAPAS !!</b><br>" +
+    JSON.stringify(phases) +
+    "<br>";
+
+  p.innerHTML = text_info;
+  infoSpace.appendChild(p);
+
   sendSolver();
   createGraphView();
 }
 
-function createLastTable(space, phase, solution) {
-  // Crear una fila de encabezado
-  var table = document.createElement("table");
-  space.appendChild(table);
+/*
+  LOGICA PRINCIPAL PARA LA CREACION DE TABLAS QUE PERTENECEN
+  A LA ULTIMA ETAPA, DONDE SE RECORRE LA SOLUCION DADA POR EL
+  BACKEND Y SE AGREGA LA TABLA AL HTML.
+*/
 
-  // Crear la cabecera de la tabla
+function createLastTable(solution, table_space, phase) {
+  var table = document.createElement("table");
+  table_space.appendChild(table);
   var title = document.createElement("caption");
   var headerRow = document.createElement("tr");
   var headerCell1 = document.createElement("th");
   var headerCell2 = document.createElement("th");
   var headerCell3 = document.createElement("th");
+
   title.textContent = "Etapa " + phase;
   headerCell1.textContent = "s";
   headerCell2.textContent = "f*" + phase + "(s)";
@@ -105,10 +170,15 @@ function createLastTable(space, phase, solution) {
   }
 }
 
+/*
+  LOGICA PRINCIPAL PARA LA CREACION DE TABLAS QUE NO PERTENECEN
+  A LA ULTIMA ETAPA, DONDE SE RECORRE LA SOLUCION DADA POR EL
+  BACKEND Y SE AGREGA CADA TABLA AL HTML.
+*/
+
 function createOtherTable(solution, table_space, phase) {
   var table = document.createElement("table");
   table_space.appendChild(table);
-
   var title = document.createElement("caption");
   var headerRow = document.createElement("tr");
   var header_s = document.createElement("th");
@@ -133,7 +203,7 @@ function createOtherTable(solution, table_space, phase) {
 
   const uniqueHeaders = new Set();
 
-  // crea headers
+  // SE CREA LOS TITULOS DE LA TABLA
   for (const key in obj_phase) {
     for (const k in obj_phase[key]["f(s,x)"]) {
       uniqueHeaders.add(k);
@@ -150,7 +220,7 @@ function createOtherTable(solution, table_space, phase) {
   headerRow.appendChild(header_x);
   table.appendChild(headerRow);
 
-  // crea datos
+  // SE CREAN LOS DATOS DENTRO DE LA TABLA
   for (const key in obj_phase) {
     var data_row = document.createElement("tr");
     var node = document.createElement("td");
@@ -179,28 +249,31 @@ function createOtherTable(solution, table_space, phase) {
   }
 }
 
-// Crea las tablas por etapa
+/*
+  SEPARA LAS LLAVES POR LA FASE A LA QUE PERTENECEN
+  CREANDO UNA TABLA ESPECIFICA PARA LA ULTIMA ETAPA
+*/
 function createTables(problem_solution) {
-  console.log(problem_solution);
   table_space = document.getElementById("Tables");
   table_space.innerHTML = "";
-  var title = document.createElement("h3");
-  title.textContent = "Tablas";
-  table_space.appendChild(title);
 
   phases_copy = { ...phases };
   var quantity_phases = Math.max(...Object.values(phases_copy));
 
   for (var i = 0; i < quantity_phases; i++) {
     if (i == quantity_phases - 1) {
-      createLastTable(table_space, i + 1, problem_solution);
+      createLastTable(problem_solution, table_space, i + 1);
     } else {
       createOtherTable(problem_solution, table_space, i + 1);
     }
   }
 }
 
-// Crea la Gráfica de los nodos
+/*
+  RECORRE LA INFORMACION DE LA CONEXION DE LOS NODOS
+  Y CREA LA VISUALIZACION GRAFICA POR MEDIO DE LA
+  LIBRERIA CYTOSCAPE.MIN.JS
+*/
 function createGraphView() {
   const copy_nodes = { ...nodes };
   const last_key_pos = Object.keys(copy_nodes).length;
@@ -212,7 +285,6 @@ function createGraphView() {
     if (data.hasOwnProperty(key)) {
       elements.push({ data: { id: key } });
 
-      // Add edges
       var edges = data[key];
       for (var target in edges) {
         if (edges.hasOwnProperty(target)) {
@@ -229,7 +301,7 @@ function createGraphView() {
     }
   }
 
-  document.getElementById("cy").style.visibility = "visible";
+  document.getElementById("cy").style.display = "block";
   var cy = cytoscape({
     container: document.getElementById("cy"),
     elements: elements,
@@ -237,7 +309,7 @@ function createGraphView() {
       {
         selector: "node",
         style: {
-          "background-color": "#66ccff",
+          "background-color": "#E3B23C",
           label: "data(id)",
         },
       },
@@ -258,8 +330,12 @@ function createGraphView() {
   });
 }
 
-// Manda el objeto como JSON al servidor Python
-// obtiene la solucion y algunos caminos posibles
+/*
+  ENVIA LA INFORMACION DE LOS NODOS Y FASES AL BACKEND
+  AL RECIBIR LA RESPUESTA DEL SERVIDOR, AGREGA LA
+  INFORMACION DE LOS MEJORES CAMINOS Y CREA LAS TABLAS
+  CORRESPONDIENTES
+*/
 
 function sendSolver() {
   var nodes_OBJ = JSON.stringify(nodes);
@@ -276,20 +352,35 @@ function sendSolver() {
   })
     .then((response) => response.json())
     .then((data) => {
-      var solucion = JSON.parse(data.solucion);
-      var camino = data.camino;
+      if (document.getElementById("Error") !== null) {
+        document.getElementById("Error").style.display = "none";
+        errorWasAdded = false;
+      }
+      var solution = JSON.parse(data.solucion);
+      var path = data.camino.replace(/,/g, "->") + " = " + solution[0]["f*(s)"];
       var path_space = document.getElementById("BestPath");
       path_space.innerHTML = "";
-      var title = document.createElement("h3");
       var text = document.createElement("p");
-      title.textContent = "Mejores Caminos";
-      text.textContent = camino;
-      path_space.appendChild(title);
+      text.textContent = path;
       path_space.appendChild(text);
 
-      createTables(solucion);
+      createTables(solution);
     })
     .catch((error) => {
-      console.error("Error al enviar los datos al servidor:", error);
+      // Verificar si el elemento ya ha sido agregado
+      if (!errorWasAdded) {
+        var infoDiv = document.getElementById("Info");
+        var divWarn = document.createElement("div");
+        var textWarn = document.createElement("p");
+        divWarn.id = "Error";
+        divWarn.innerHTML = "";
+
+        (textWarn.textContent =
+          "ERROR AL ENVIAR DATOS AL SERVIDOR, VERIFIQUE LAS CONEXIONES DE LA LISTA ADYACENTE O LA CONEXIÓN AL SERVIDOR"),
+          error.message;
+        divWarn.appendChild(textWarn);
+        infoDiv.parentNode.insertBefore(divWarn, infoDiv.nextSibling);
+        errorWasAdded = true;
+      }
     });
 }
